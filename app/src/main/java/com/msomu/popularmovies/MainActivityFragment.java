3abb3df4cf878bde793b89b6541dc297f5fbc05f
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-02-10 11:59:24 +0530

    showing recyler view

--- app/src/main/java/com/msomu/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/msomu/popularmovies/MainActivityFragment.java
@@ -1,22 +1,51 @@
 package com.msomu.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements RecyclerViewAdapter.OnItemClickListener {
 
+    private static List<ViewModel> items = new ArrayList<>();
+
+    static {
+        for (int i = 1; i <= 10; i++) {
+            items.add(new ViewModel("Item " + i, "http://lorempixel.com/500/500/animals/" + i));
+        }
+    }
+
+    private View rootView;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initRecyclerView();
+        return rootView;
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(items);
+        adapter.setOnItemClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, ViewModel viewModel) {
+
     }
 }
